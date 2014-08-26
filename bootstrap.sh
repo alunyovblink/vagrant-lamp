d0ce957616b10de9d53d559c92b6cf34546bd96f
Alexandr Lunyov <alexandr.lunyov@gmail.com>
2014-08-26 17:06:31 +0300

    Updated shell script for getting prepared virtual machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# Installing curl
 sudo apt-get install -y curl
+
+# Installing and setting up RVM and latest Ruby
 sudo \curl -L https://get.rvm.io | bash -s stable --ruby=1.9.3
 source /usr/local/rvm/scripts/rvm
 source /etc/profile.d/rvm.sh
-sudo gem install chef --no-ri --no-rdoc
+
+# Installing chef
+gem install chef --no-ri --no-rdoc
